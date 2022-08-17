1116bcc1d7a28a2c56894497ad4a309c0444c458
Jodee-Mohamed8 <jojomohamed288@gmail.com>
2022-08-18 01:59:03 +0300

    cw7 is done

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+function BMI(weight = 50, height = 1.60){
+    let result=(weight / (height * height))
+    return result ;
+   
+}
+console.log(BMI(55,1.65));
+console.log(BMI());
+
+function Status(bmi){
+    let div= document.getElementById ("result")
+    
+    if (bmi<18.5){
+        div.style.color = "orange"
+        return "لديك نقص في الوزن"
+
+    }
+    else if (bmi>=18.5&&bmi<25){
+         div.style.color = "green"
+        return "وزنك صحي"
+      
+    }
+    else if (bmi>=25){
+        div.style.color = "red"
+        return "لديك زيادة في الوزن"
+    }
+}
+
+function calculate() {
+    let weight = document.getElementById ("weight").value
+    let height = document.getElementById ("height").value
+    let bmi = BMI(weight,height)
+
+    let desc = (Status(bmi))
+
+    let div = document.getElementById ("result") ;
+     div.innerText = bmi + "==" +desc   
+    
+}
+
+
 
